d698815a54f876038a0c49bce3de62325f66dd44
Justin Tu <coppolavina@gmail.com>
2022-01-05 08:54:03 +0000

    validation

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,22 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+# Add the following code directly inside the Store model (class): has_many :employees
+# Add the following code directly inside the Employee model (class): belongs_to :store
+
+# Add some data into employees. Here's an example of one (note how it differs from how you create stores): 
+# @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+# Go ahead and create some more employees using the create method. You can do this by 
+# making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the @store# instance variables that you defined in previous exercises. 
+# Create a bunch under @store1 (Burnaby) and @store2 (Richmond). Eg: @store1.employees.create(...).
+
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 60)
+@store1.employees.create(first_name: "Hint", last_name: "Shu", hourly_rate: 90)
+@store2.employees.create(first_name: "Hello", last_name: "Vivi", hourly_rate: 15)
+@store2.employees.create(first_name: "Ui", last_name: "Ux", hourly_rate: 20)
+
+puts Employee.count
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true }
 
+  validate :at_least_one_mens_or_womens_apparel
+
+  def at_least_one_mens_or_womens_apparel
+    if !mens_apparel.present? && !womens_apparel.present?
+      errors.add(:mens_apparel, "must have 1 men or womens apparel")
+    end
+  end
 end
