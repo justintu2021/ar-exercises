04dc7a06e202190d77a796b58f79af7d5c763093
Justin Tu <coppolavina@gmail.com>
2022-01-05 08:29:56 +0000

    sum, avergage, filter by chain where and count

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,19 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# Output the total revenue for the entire company (all stores), using Active Record's .sum calculation method.
+# On the next line, also output the average annual revenue for all stores.
+# Output the number of stores that are generating $1M or more in annual sales. Hint: Chain together where and size (or count) Active Record methods.
+
+
+@total = Store.sum(:annual_revenue)
+@average= @total/Store.count
+
+# puts @total
+# puts @average
+
+
+@filter_store = Store.where('annual_revenue > 1000000').count
+
+# puts @filter_store 
